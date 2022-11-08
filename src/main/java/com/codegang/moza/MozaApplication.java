cba8b899792292bc95e6e8352fca459bdaa22308
Daniel Lee <idaniel@macAir.local>
2022-11-09 00:50:37 +0900

    initial commit

---
+++ src/main/java/com/codegang/moza/MozaApplication.java
@@ -0,0 +1,13 @@
+package com.codegang.moza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codegang/moza/MozaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegang.moza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
